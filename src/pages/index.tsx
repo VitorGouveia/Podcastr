126badc6f5c038cff29fc35f55980107bf694fdf
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-04-20 10:28:03 -0300

    Adds Static Site Generation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,35 @@
-export default function Home() {
+import { GetStaticProps } from "next"
+
+interface HomeProps {
+  id: string
+  title: string
+  members: string
+  published_at: string
+  thumbnail: string
+  description: string
+  file: {
+    url: string
+    type: string
+    duration: number
+  }
+}
+
+export default function Home({ episodes }) {
+
   return (
     <h1>index</h1>
   )
+}
+
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await fetch("http://localhost:3333/episodes")
+  const data = await response.json()
+
+  return {
+    props: {
+      episodes: data
+    },
+    revalidate: 60 * 60 * 8 // 8hrs
+  }
 }
