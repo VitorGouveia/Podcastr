01379dfcb3a6d167c23002cef792a7221d896340
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-04-23 12:08:34 -0300

    Moves Player context to its own file

---
+++ src/contexts/PlayerContext.tsx
@@ -0,0 +1,46 @@
+import { createContext, useState, FC } from "react"
+
+type Episode = {
+  title: string
+  members: string
+  thumbnail: string
+  duration: number
+  url: string
+}
+
+type PlayerContextData = {
+  episodeList: Episode[]
+  currentEpisodeIndex: number
+  isPlaying: boolean
+  play: (episode: Episode) => void
+  togglePlay: () => void
+  setPlayingState: (state: boolean) => void
+}
+
+export const PlayerContext = createContext({} as PlayerContextData)
+
+export const PlayerContextProvider: FC = ({ children }) => {
+  const [episodeList, setEpisodeList] = useState([])
+  const [currentEpisodeIndex, setcurrentEpisodeIndex] = useState(0)
+  const [isPlaying, setIsPlaying] = useState(false)
+
+  function play(episode) {
+    setEpisodeList([episode])
+    setcurrentEpisodeIndex(0)
+    setIsPlaying(true)
+  }
+
+  function togglePlay() {
+    setIsPlaying(!isPlaying)
+  }
+
+  function setPlayingState(state: boolean) {
+    setIsPlaying(state)
+  }
+
+  return (
+    <PlayerContext.Provider value={{ episodeList, currentEpisodeIndex, isPlaying, play, togglePlay, setPlayingState }}>
+      {children}
+    </PlayerContext.Provider>
+  )
+}
