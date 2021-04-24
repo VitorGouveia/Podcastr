ad436a2b950930be66dd60a0807200a98427fd48
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-04-24 13:34:43 -0300

    Adds shuffle feature + minor css player addons

--- src/components/Player/index.tsx
+++ src/components/Player/index.tsx
@@ -11,7 +11,7 @@ import styles from "./styles.module.scss"
 export function Player() {
   const audioRef = useRef<HTMLAudioElement>(null)
 
-  const { episodeList, currentEpisodeIndex, isPlaying, isLooping, togglePlay, setPlayingState, playNext, playPrevious, hasNext, hasPrevious } = usePlayer()
+  const { episodeList, currentEpisodeIndex, isPlaying, isLooping, isShuffling, togglePlay, toggleLoop, toggleShuffle, setPlayingState, playNext, playPrevious, hasNext, hasPrevious } = usePlayer()
 
   useEffect(() => {
     if(!audioRef.current) {
@@ -81,7 +81,7 @@ export function Player() {
         ) }
 
         <div className={styles.buttons}>
-          <button type="button" disabled={!episode}>
+          <button type="button" disabled={!episode || episodeList.length == 1} onClick={toggleShuffle} className={isShuffling ? styles.isActive : ""}>
             <img src="/shuffle.svg" alt="Embaralhar"/>
           </button>
           <button type="button" onClick={playPrevious} disabled={!episode || !hasPrevious}>
@@ -96,7 +96,7 @@ export function Player() {
           <button type="button" onClick={playNext} disabled={!episode || !hasNext}>
             <img src="/play-next.svg" alt="Tocar próxima"/>
           </button>
-          <button type="button" disabled={!episode}>
+          <button type="button" disabled={!episode} onClick={toggleLoop} className={isLooping ? styles.isActive : ""}>
             <img src="/repeat.svg" alt="Repetir"/>
           </button>
         </div>
--- src/contexts/PlayerContext.tsx
+++ src/contexts/PlayerContext.tsx
@@ -13,8 +13,10 @@ type PlayerContextData = {
   currentEpisodeIndex: number
   isPlaying: boolean
   isLooping: boolean
+  isShuffling: boolean
   play: (episode: Episode) => void
   togglePlay: () => void
+  toggleShuffle: () => void
   toggleLoop: () => void
   setPlayingState: (state: boolean) => void
   playList: (list: Episode[], index: number) => void
@@ -35,6 +37,7 @@ export const PlayerContextProvider: FC<PlayerContextProviderProps> = ({ children
   const [currentEpisodeIndex, setcurrentEpisodeIndex] = useState(0)
   const [isPlaying, setIsPlaying] = useState(false)
   const [isLooping, setIsLooping] = useState(false)
+  const [isShuffling, setIsShuffling] = useState(false)
 
   function play(episode: Episode) {
     setEpisodeList([episode])
@@ -56,6 +59,10 @@ export const PlayerContextProvider: FC<PlayerContextProviderProps> = ({ children
     setIsLooping(!isLooping)
   }
 
+  function toggleShuffle() {
+    setIsShuffling(!isShuffling)
+  }
+
   function setPlayingState(state: boolean) {
     setIsPlaying(state)
   }
@@ -64,7 +71,10 @@ export const PlayerContextProvider: FC<PlayerContextProviderProps> = ({ children
   const hasNext = (currentEpisodeIndex + 1) < episodeList.length
 
   function playNext() {
-    if(hasNext) {
+    if(isShuffling) {
+      const nextRandomEpisodeIndex = Math.floor(Math.random() * episodeList.length)
+      setcurrentEpisodeIndex(nextRandomEpisodeIndex)
+    } else if(hasNext) {
       setcurrentEpisodeIndex(currentEpisodeIndex + 1)
     }
   }
@@ -81,9 +91,11 @@ export const PlayerContextProvider: FC<PlayerContextProviderProps> = ({ children
       currentEpisodeIndex, 
       isPlaying, 
       isLooping,
+      isShuffling,
       play, 
       togglePlay, 
       toggleLoop,
+      toggleShuffle,
       setPlayingState,
       playList,
       hasNext,
