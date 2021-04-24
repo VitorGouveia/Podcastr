5c96d969687df748e71f9c567fcc2a3e73e87b77
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-04-24 14:21:32 -0300

    Adds localStorage light theme

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,7 @@
 import "../styles/global.scss"
 
+import { useEffect } from "react"
+
 import { Header } from "../components/Header"
 import { Player } from "../components/Player"
 
@@ -8,6 +10,14 @@ import { PlayerContextProvider } from "../contexts/PlayerContext"
 import styles from "../styles/app.module.scss"
 
 function MyApp({ Component, pageProps }) {
+  useEffect(() => {
+    localStorage.setItem("theme", "light")
+
+    const currentTheme = localStorage.getItem("theme")
+
+    document.body.classList.toggle(currentTheme)
+  }, [])
+
   return(
     <PlayerContextProvider>
       <div className={styles.wrapper}>
