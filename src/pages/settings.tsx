3448646a3a7b602ec601c3155965d27378b99450
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-04-27 20:14:25 -0300

    Adds theme card in settings page

--- src/pages/settings.tsx
+++ src/pages/settings.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head"
-import Image from "next/image"
 
 import styles from "../styles/settings.module.scss"
 
@@ -13,6 +12,31 @@ export default function Settings() {
 
       <section className={styles.themeSettings}>
         <h2>Temas</h2>
+
+        <ul>
+          <form>
+            <li>
+              <label>
+                <img width="192" height="192" src="light.jpg" alt="Tema Claro" />
+
+                <div className={styles.themeDetails}>
+                  <span>Tema Claro</span>
+                  <input type="radio" />
+                </div>
+              </label>
+            </li>
+            <li>
+              <label>
+                <img width="192" height="192" src="dark.png" alt="Tema Escuro" />
+
+                <div className={styles.themeDetails}>
+                  <span>Tema Escuro</span>
+                  <input type="radio" />
+                </div>
+              </label>
+            </li>
+          </form>
+        </ul>
       </section>
     </div>
   )
