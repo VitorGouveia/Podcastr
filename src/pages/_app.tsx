51ecaabeece7e868238aa7622e808942f4317268
VitorGouveia <vitor.gouveia@aluno.etecjrm.com.br>
2021-04-27 18:26:45 -0300

    Adds tsconfig code paths

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -7,7 +7,7 @@ import { Player } from "../components/Player"
 
 import { PlayerContextProvider } from "../contexts/PlayerContext"
 
-import styles from "../styles/app.module.scss"
+import styles from "@styles/app.module.scss"
 
 function MyApp({ Component, pageProps }) {
   useEffect(() => {
